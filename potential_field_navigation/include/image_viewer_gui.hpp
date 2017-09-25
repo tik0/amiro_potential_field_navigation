3a5cd53dca051947d4a73603b73c701486a38103
Daniel Rudolph <drudolph@techfak.uni-bielefeld.de>
2017-09-25 09:56:55 +0200

    added first gui design.

--- potential_field_navigation/include/image_viewer_gui.hpp
+++ potential_field_navigation/include/image_viewer_gui.hpp
@@ -1,5 +1,12 @@
+// ============================================================================
+// Name        : image_viewer_gui.hpp
+// Author      : Daniel Rudolph <drudolph@techfak.uni-bielefeld.de>
+// Description : Creates a GUI to view two different images.
+// ============================================================================
+
 // Qt5 Widgets
 #include <QWidget>
+#include <QObject>
 #include <QMainWindow>
 #include <QFileDialog>
 #include <QApplication>
@@ -7,6 +14,7 @@
 #include <QLabel>
 #include <QCheckBox>
 #include <QLineEdit>
+#include <QSlider>
 
 #include <boost/thread.hpp>
 
@@ -23,27 +31,45 @@ class ImageViewGUI : public QWidget {
 //Q_OBJECT
 
 public:
+
   explicit ImageViewGUI(QWidget *parent = 0);
 
   ~ImageViewGUI();
-
-public:
 
   QImage mat2QImage(const cv::Mat &mat) {
     return QImage((const unsigned char *) (mat.data), mat.cols, mat.rows, QImage::Format_RGB888);
   }
 
+private:
+
+
   void spin();
+
+  void transparencySliderValueChanged1(int value);
+
+  void transparencySliderValueChanged2(int value);
+
+  void clickedLoadImageButton1();
+
+  void clickedLoadImageButton2();
 
 private:
 
   bool shutdown_required;
   boost::thread thread;
 
-  QLineEdit *text1;
-  QLineEdit *text2;
-  QLabel *image_label;
-
+  QLineEdit *textBox1;
+  QLineEdit *textBox2;
+  QPushButton *loadImageButton1;
+  QPushButton *loadImageButton2;
+  QSlider *transparencySlider1;
+  QSlider *transparencySlider2;
+  QLabel *transparencySliderLabel1;
+  QLabel *transparencySliderLabel2;
+  QLabel *transparencySliderLabelInfo1;
+  QLabel *transparencySliderLabelInfo2;
+  QLabel *imageLabel1;
+  QLabel *imageLabel2;
 };
 
 #endif //_IMAGE_VIEWER_GUI_HPP_
--- potential_field_navigation/src/image_viewer_node.cpp
+++ potential_field_navigation/src/image_viewer_node.cpp
@@ -20,7 +20,10 @@
 
 
 #define button_height 30
-#define button_width 100
+#define button_width 120
+#define text_label_width 180
+#define slider_width 150
+#define slider_label_info_width 50
 
 #define line_edit_height 30
 #define line_edit_width 200
@@ -33,26 +36,79 @@ using namespace std;
 ImageViewGUI::ImageViewGUI(QWidget *parent) : QWidget(parent), shutdown_required(false), thread(&ImageViewGUI::spin, this) {
 
 
-  image_label = new QLabel(this);
-  image_label->setGeometry(200, 0, 100, 100);
-  image_label->setText(QString::fromStdString("image label"));
+  textBox1 = new QLineEdit(this);
+  textBox1->setGeometry(0, 0, line_edit_width, line_edit_height);
+  textBox1->setText("text1");
 
-  text1 = new QLineEdit(this);
-  text1->setGeometry(0, 0, line_edit_width, line_edit_height);
-  text1->setText("text1");
+  loadImageButton1 = new QPushButton(this);
+  loadImageButton1->setGeometry(textBox1->pos().x()+textBox1->width(), 0, button_width, button_height);
+  loadImageButton1->setText("loadImageButton1");
+  QObject::connect(loadImageButton1, &QPushButton::clicked, this, &ImageViewGUI::clickedLoadImageButton1);
 
-  text2 = new QLineEdit(this);
-  text2->setGeometry(0, line_edit_height, line_edit_width, line_edit_height);
-  text2->setText("text2");
+  transparencySliderLabel1 = new QLabel(this);
+  transparencySliderLabel1->setGeometry(line_edit_width + button_width, 0, text_label_width, button_height);
+  transparencySliderLabel1->setText("Transparency 0% to 100%");
+
+  transparencySlider1 = new QSlider(Qt::Horizontal, this);
+  transparencySlider1->setGeometry(line_edit_width + button_width + text_label_width, 0, slider_width, button_height);
+  transparencySlider1->setMinimum(0);
+  transparencySlider1->setMaximum(100);
+  transparencySlider1->setSliderPosition(70);
+  QObject::connect(transparencySlider1, &QSlider::sliderMoved, this, &ImageViewGUI::transparencySliderValueChanged1);
+
+  transparencySliderLabelInfo1 = new QLabel(this);
+  transparencySliderLabelInfo1->setGeometry(line_edit_width + button_width + text_label_width + slider_width, 0, slider_label_info_width, button_height);
+  transparencySliderLabelInfo1->setText(QString::number(transparencySlider1->value()));
+
+  textBox2 = new QLineEdit(this);
+  textBox2->setGeometry(0, line_edit_height, line_edit_width, line_edit_height);
+  textBox2->setText("text2");
+
+  loadImageButton2 = new QPushButton(this);
+  loadImageButton2->setGeometry(line_edit_width, line_edit_height, button_width, button_height);
+  loadImageButton2->setText("loadImageButton2");
+  QObject::connect(loadImageButton2, &QPushButton::clicked, this, &ImageViewGUI::clickedLoadImageButton2);
+
+  transparencySliderLabel2 = new QLabel(this);
+  transparencySliderLabel2->setGeometry(line_edit_width + button_width, button_height, text_label_width, button_height);
+  transparencySliderLabel2->setText("Transparency 0% to 100%");
+
+  transparencySlider2 = new QSlider(Qt::Horizontal, this);
+  transparencySlider2->setGeometry(line_edit_width + button_width + text_label_width, button_height, slider_width, button_height);
+  transparencySlider2->setMinimum(0);
+  transparencySlider2->setMaximum(100);
+  transparencySlider2->setSliderPosition(70);
+  QObject::connect(transparencySlider2, &QSlider::sliderMoved, this, &ImageViewGUI::transparencySliderValueChanged2);
+
+  transparencySliderLabelInfo2 = new QLabel(this);
+  transparencySliderLabelInfo2->setGeometry(line_edit_width + button_width + text_label_width + slider_width, button_height, slider_label_info_width, button_height);
+  transparencySliderLabelInfo2->setText(QString::number(transparencySlider2->value()));
 
 
-  this->setMinimumSize(500, 500);
+  imageLabel1 = new QLabel(this);
+  imageLabel1->setGeometry(0, 2 * line_edit_height, 1000, 1000);
+  imageLabel1->setText(QString::fromStdString("image label1"));
+
+  imageLabel2 = new QLabel(this);
+  imageLabel2->setGeometry(0, 2 * line_edit_height, 1000, 1000);
+  imageLabel2->setText(QString::fromStdString("image label2"));
+
   this->setWindowTitle(QString::fromStdString(ros::this_node::getName()));
 }
 
 ImageViewGUI::~ImageViewGUI() {
-  delete text1;
-  delete text2;
+  delete textBox1;
+  delete textBox2;
+  delete loadImageButton1;
+  delete loadImageButton2;
+  delete transparencySlider1;
+  delete transparencySlider2;
+  delete transparencySliderLabel1;
+  delete transparencySliderLabel2;
+  delete transparencySliderLabelInfo1;
+  delete transparencySliderLabelInfo2;
+  delete imageLabel1;
+  delete imageLabel2;
 
   shutdown_required = true;
   thread.join();
@@ -69,6 +125,21 @@ void ImageViewGUI::spin() {
   QApplication::quit();
 }
 
+void ImageViewGUI::clickedLoadImageButton1() {
+  ROS_INFO("[%s] clicked loadImageButton1", ros::this_node::getName().c_str());
+}
+
+void ImageViewGUI::clickedLoadImageButton2() {
+  ROS_INFO("[%s] clicked loadImageButton2", ros::this_node::getName().c_str());
+}
+
+void ImageViewGUI::transparencySliderValueChanged1(int value) {
+  this->transparencySliderLabelInfo1->setText(QString::number(value));
+}
+
+void ImageViewGUI::transparencySliderValueChanged2(int value) {
+  this->transparencySliderLabelInfo2->setText(QString::number(value));
+}
 
 void imageCallback1(const sensor_msgs::ImageConstPtr &msg) {
   image1 = cv_bridge::toCvShare(msg, "bgr8")->image;
